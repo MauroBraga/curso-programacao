3ea80d6885b0a3d2a0ec0383b9b33e471f31bccf
mauro <mrb0305@gmail.com>
2025-03-23 19:13:06 -0300

    Commit inicial

---
+++ src/test/java/br/com/mrb/cursoprogramacao/CursoProgramacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mrb.cursoprogramacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoProgramacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
